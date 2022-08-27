1b1c416656ab4ff9eb9e3989c927d09285946a20
Aliskhan <ag@farcana.com>
2022-08-27 15:42:46 +0300

    first commit

---
+++ src/actions.js
@@ -0,0 +1,14 @@
+export const loadTodos = () => {
+    return (dispatch) => {
+        dispatch({ type: 'todos/start' })
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => {
+                dispatch({
+                    type: 'todos/items',
+                    payload: json
+                })
+            })
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -2,16 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import thunk from 'redux-thunk'
+
+const initialState = {
+    todos: [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+    ],
+    loading: false
+};
+
+const reducer = (state = initialState, action) =>{
+    switch (action.type){
+        case 'todos/start':
+            return {
+                ...state,
+                loading: true
+            }
+        case 'todos/items':
+            return {
+                ...state,
+                todos: action.payload,
+                loading: false
+            }
+
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
